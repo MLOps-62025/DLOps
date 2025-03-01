e1795816f3c707c15b6c2ae24042105da509fb51
Pranita <141933843+pranitab07@users.noreply.github.com>
2025-03-01 10:55:06 +0530

    Day 1 DL

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='implementing dl models and deploying into production environment',
+    author='Pranita',
+    license='MIT',
+)
